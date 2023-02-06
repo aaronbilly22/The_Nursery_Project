e7da44126d22237359b5b50e9b9d5b75f9994283
unknown <billonea@mcmaster.ca>
2023-02-06 14:32:17 -0500

    conveyor control

--- src/conveyor_control/relay_test/relay_test.ino
+++ src/conveyor_control/relay_test/relay_test.ino
@@ -1,56 +1,20 @@
-// //Normally open is side with 2; Top
-// //Normally closed is side with 2; Bottom
-// //Coil pos/neg are side with 3; Top and Bottom
-// //Common Output is side with 3; Middle
-
-// #define coil_1 3
+const int relay = 3;
+const int stop = 4;
 
 
-
-// void setup() {
-//   pinMode(coil_1, OUTPUT);
-
-//   Serial.begin(9600);
-  
-
-
-// }
-
-// void loop() {
-
-
-//   digitalWrite(coil_1,1);
-//   delay(5000);
-//   digitalWrite(coil_1,0);
-//   delay(1000);
-  
-  
-// }
-
-#include <ezButton.h>
-#define led 8
-
-ezButton button2(10);
 
 void setup() {
-  button2.setDebounceTime(50);
-  pinMode(led, OUTPUT);
+  pinMode(relay, OUTPUT);
+  pinMode(stop, INPUT);
 
   Serial.begin(9600);
-  
-  while (!button2.isPressed()){
-    button2.loop();
-  }
-
 }
 
 void loop() {
 
-  digitalWrite(led,1);
-  delay(1000);
-  digitalWrite(led,0);
-  delay(1000);
-  
+  digitalWrite(relay,HIGH);
+  if(digitalRead(stop)==HIGH){
+    digitalWrite(relay,LOW);
+  }
   
 }
-
