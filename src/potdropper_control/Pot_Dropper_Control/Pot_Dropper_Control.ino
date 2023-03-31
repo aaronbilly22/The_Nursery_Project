6ba57497122025c79ea9d22f01137def49778509
juanSMoncada <moncadaj@mcmaster.ca>
2023-03-31 19:28:42 -0400

    new pot dropper code

--- src/potdropper_control/Pot_Dropper_Control/Pot_Dropper_Control.ino
+++ src/potdropper_control/Pot_Dropper_Control/Pot_Dropper_Control.ino
@@ -1,12 +1,20 @@
-#define trigPin 10
-#define echoPin 13
+#include <Servo.h>
+#define LIMIT_SWITCH_PIN_L A0;
+#define LIMIT_SWITCH_PIN_R A2;  
+#define servoPin1 A1;
+#define servoPin2 A3;
 const int stepPin1 = 2; 
 const int dirPin1 =3; 
 const int stepPin2 = 5; 
 const int dirPin2 = 6; 
-
+Servo Servo1; 
+Servo Servo2;
 void setup() {
   Serial.begin (9600);
+  pinMode(LIMIT_SWITCH_PIN_L, INPUT);
+  Servo1.attach(servoPin1); 
+  pinMode(LIMIT_SWITCH_PIN_R, INPUT);
+  Servo2.attach(servoPin2); 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(11, OUTPUT);//stop LED
@@ -31,29 +39,30 @@ void loop() {
   
   duration = pulseIn(echoPin, HIGH);
   distance = (duration / 2) * 0.0344;
-  Serial.print("Distance = ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(10);
 
-  if (distance < 15){
-  for(int i=0;i<5;i++){
-  digitalWrite(11, HIGH);
-  digitalWrite(dirPin1,HIGH);
-  digitalWrite(dirPin2,HIGH);
-    for(int x = 0; x < 200; x++) {
-      digitalWrite(stepPin1,HIGH); 
-      digitalWrite(stepPin2,HIGH);
-      delayMicroseconds(600); 
-      digitalWrite(stepPin1,LOW); 
-      digitalWrite(stepPin2,LOW); 
-      delayMicroseconds(600); 
-    }
-  delay(825);
-  digitalWrite(11, LOW);
-  delay(825);
-  }
+   Servo1.write(0);
+   Servo2.write(0); 
+   Serial.println(digitalRead(LIMIT_SWITCH_PIN_L)&&digitalRead(LIMIT_SWITCH_PIN_R));
+   if (digitalRead(LIMIT_SWITCH_PIN_L) == HIGH && digitalRead(LIMIT_SWITCH_PIN_R) == HIGH)
+   {
+      Serial.println(digitalRead(LIMIT_SWITCH_PIN_L)&&digitalRead(LIMIT_SWITCH_PIN_R));
+      digitalWrite(11, HIGH);
+      Servo1.write(90);
+      Servo2.write(90);   
+      digitalWrite(dirPin1,HIGH);
+      digitalWrite(dirPin2,HIGH);
+      for(int x = 0; x < 200; x++) {
+        digitalWrite(stepPin1,HIGH); 
+        digitalWrite(stepPin2,HIGH);
+        delayMicroseconds(600); 
+        digitalWrite(stepPin1,LOW); 
+        digitalWrite(stepPin2,LOW); 
+      delayMicroseconds(600);
+      if (distance < 5){
+      // Make servo go to 90 degrees 
+      Servo1.write(0); 
+      Servo2.write(0);
+      }
+   }
+
   
-  }
-
-}
